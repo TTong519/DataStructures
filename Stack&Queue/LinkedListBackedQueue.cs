1874c18e3f882ba61a0c7cceab99acac1f6d6f4f
tyler.tong <tyler.tong@GMR-118-1-3>
2025-02-27 19:02:26 -0800

    did linked list queue todo a test

---
+++ Stack&Queue/LinkedListBackedQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue
+{
+    class LinkedListBackedQueue<T>
+    {
+        public int Count { get { return data.Count; } }
+        private SinglyLinkedList<T> data;
+        public LinkedListBackedQueue(T value, T next)
+        {
+            data.AddFirst(next);
+            data.AddLast(value);
+        }
+        public void Enqueue(T value)
+        {
+            data.AddFirst(value);
+        }
+        public T Dequeue()
+        {
+            T toReturn;
+            toReturn = data.Tail.Value;
+            data.RemoveLast();
+            return toReturn;
+        }
+        public T Peek()
+        {
+            return data.Tail.Value;
+        }
+        public void Clear()
+        {
+            data.Clear();
+        }
+        public bool IsEmpty()
+        {
+            if (data.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
