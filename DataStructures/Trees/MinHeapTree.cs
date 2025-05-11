6723cce09d6877cb456ae534546428da1035402a
tyler.tong <tyler.tong@GMR-118-1-1>
2025-05-10 17:30:22 -0700

    fja;lsdgj

--- DataStructures/Trees/MinHeapTree.cs
+++ DataStructures/Trees/MinHeapTree.cs
@@ -8,7 +8,7 @@ namespace Stack_Queue.Trees
 {
     public class MinHeapTree<T> where T : IComparable<T>
     {
-        T[] data;
+        private T[] data;
         public int Count { get; private set; }
         public int Size { get; private set; }
         public MinHeapTree()
@@ -17,9 +17,39 @@ namespace Stack_Queue.Trees
             Count = 0;
             Size = 10;
         }
-        private void HeapifyUp( int index)
+        private void HeapifyUp(int index)
         {
-
+            if (index == 0)
+            {
+                return;
+            }
+            if (data[index].CompareTo(data[(index-1)/2]) < 0)
+            {
+                T temp = data[(index-1)/2];
+                data[(index-1)/2] = data[index];
+                data[index] = temp;
+                HeapifyUp((index-1)/2);
+            }
+        }
+        public void Add(T item)
+        {
+            if(Count == Size)
+            {
+                T[] data2 = new T[Size*2];
+                for (int i = 0; i < Size; i++)
+                {
+                    data2[i] = data[i];
+                }
+                data = data2;
+                Size = Size*2;
+            }
+            data[Count] = item;
+            HeapifyUp(Count);
+            Count++;
+        }
+        public T Peek()
+        { 
+            return data[Count-1];
         }
     }
 }
