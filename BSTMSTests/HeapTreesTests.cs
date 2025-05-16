7b0ed107f9053bdf089580d9d41436a3090448b9
Tyler.Tong <Tyler.Tong@GMR-118-2-3>
2025-05-15 19:01:05 -0700

    heapsort no work

--- BSTMSTests/HeapTreesTests.cs
+++ BSTMSTests/HeapTreesTests.cs
@@ -3,16 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DataStructures.Trees;
 namespace DataStructuresTests
 {
     [TestClass]
-    internal class HeapTreesTests
+    public class HeapTests
     {
         [TestMethod]
-        void MinHeapAddTests()
+        [DataRow(7, 5, 2, 8, 1, 92, 0, -100, -7)]
+        public void Add(params int[] input)
         {
-
+            HeapTree<int> tree = new HeapTree<int>();
+            int fittest = input[0];
+            foreach(int i in input)
+            {
+                tree.Add(i);
+                if(i < fittest)
+                {
+                    fittest = i;
+                }
+                if(tree.Root != fittest)
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+        [TestMethod]
+        [DataRow(7, 5, 2, 8, 1, 92, 0, -100, -7)]
+        public void Remove(params int[] input)
+        {
+            HeapTree<int> tree = new HeapTree<int>();
+            List<int> removed = new List<int>();
+            foreach(int i in input)
+            {
+                tree.Add(i);
+            }
+            for(int i = 0; i < tree.Count; i++)
+            {
+                removed.Add(tree.Pop());
+            }
+            Array.Sort(input);
+            CollectionAssert.AreEqual(input, removed);
+        }
+        [TestMethod]
+        [DataRow(36412799)]
+        [DataRow(834047848)]
+        [DataRow(300173266)]
+        [DataRow(1241957440)]
+        [DataRow(50322915)]
+        [DataRow(25004)]
+        public void Add(int seed)
+        {
+            Random random = new Random();
+            int[] array = new int[random.Next(20, 50)];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next();
+            }
+            Add(array);
         }
     }
 }
---
+++ DataStructures/Trees/HeapTree.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Trees
+{
+    public class HeapTree<T> where T : IComparable<T>
+    {
+        private T[] data;
+        public T Root { get { return data[0]; } }
+        public int Count { get; private set; }
+        public int Capacity { get { return data.Length; } }
+        public HeapTree()
+        {
+            data = new T[10];
+            Count = 0;
+        }
+        private void HeapifyUp(int index)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+            if (data[index].CompareTo(data[(index-1)/2]) < 0)
+            {
+                T temp = data[(index-1)/2];
+                data[(index-1)/2] = data[index];
+                data[index] = temp;
+                HeapifyUp((index-1)/2);
+            }
+        }
+        private void HeapifyDown(int index)
+        {
+            int leftIndex = 2 * index + 1;
+            int rightIndex = 2 * index + 2;
+            bool left = false;
+            bool right = false;
+            if (leftIndex > Count)
+            {
+                left = true;
+            }
+            if (rightIndex > Count)
+            {
+                right = true;
+            }
+            if (left && right)
+            {
+                return;
+            }
+            else if (left)
+            {
+                T temp = data[index];
+                data[index] = data[rightIndex];
+                data[rightIndex] = temp;
+                HeapifyDown(rightIndex);
+            }
+            else if (right)
+            {
+                T temp = data[index];
+                data[index] = data[leftIndex];
+                data[leftIndex] = temp;
+                HeapifyDown(leftIndex);
+            }
+            else
+            {
+                if (data[leftIndex].CompareTo(data[rightIndex]) > 0)
+                {
+                    T temp = data[index];
+                    data[index] = data[leftIndex];
+                    data[leftIndex] = temp;
+                    HeapifyDown(leftIndex);
+                }
+                else
+                {
+                    T temp = data[index];
+                    data[index] = data[rightIndex];
+                    data[rightIndex] = temp;
+                    HeapifyDown(rightIndex);
+                }
+            }
+        }
+        public void Add(T item)
+        {
+            if(Count == Capacity)
+            {
+                T[] data2 = new T[Capacity*2];
+                for (int i = 0; i < Capacity; i++)
+                {
+                    data2[i] = data[i];
+                }
+                data = data2;
+            }
+            data[Count] = item;
+            HeapifyUp(Count);
+            Count++;
+        }
+        public T Pop()
+        {
+            T toReturn = Root;
+            data[0] = data[Count - 1];
+            data[Count - 1] = default;
+            Count--;
+            HeapifyDown(0);
+            return toReturn;
+        }
+    }
+}
