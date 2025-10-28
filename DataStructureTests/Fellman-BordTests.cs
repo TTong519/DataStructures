60f81ffbc31eed2005ec89044cc05946831979a6
Tyler.Tong <Tyler.Tong@GMR-118-2-3>
2025-10-27 19:00:41 -0700

    Enhance random selection and graph generation logic
    - Modified `SelectRandomly` method to include a `seed` parameter for reproducible random number generation and added error handling for empty lists.
    - Improved `generateGraph` method to prevent duplicate values in `unconnectedVals` and added more edges with random weights for better graph connectivity.
    - Updated `GraphTests` to use the `Search` method for adding edges, ensuring correct vertex references.
    
    These changes enhance error handling, improve graph generation, and increase the reliability of graph tests.

--- DataStructureTests/Fellman-BordTests.cs
+++ DataStructureTests/Fellman-BordTests.cs
@@ -7,18 +7,28 @@ namespace DataStructuresTests;
 [TestClass]
 public class FellmanBordTests
 {
-    public T SelectRandomly<T>(List<T> list)
+    int Seed = 0;
+
+    public T SelectRandomly<T>(List<T> list, int seed)
     {
-        return list[new Random().Next(0, list.Count)];
+        if(list.Count == 0)
+        {
+            throw new Exception("Cannot select from an empty list");
+        }
+        return list[new Random(seed).Next(0, list.Count - 1)];
     }
     [TestMethod]
     public void FellmanBordTest()
     {
-
+        (DirectedWeightedGraph<int>, bool) thing = generateGraph();
+        if (FellmanBord<int>.Compute(thing.Item1, thing.Item1.Vertices[0].Value) != thing.Item2)
+        {
+            Assert.Fail("Fellman-Bord algorithm did not return the expected result.");
+        }
     }
     (DirectedWeightedGraph<int>, bool) generateGraph()
     {
-        Random rand = new Random();
+        Random rand = new Random(Seed);
         var graph = new DirectedWeightedGraph<int>();
         bool isCycle = false;
         List<(int value, int count)> unconnectedVals = new List<(int, int)>();
@@ -30,7 +40,13 @@ public class FellmanBordTests
         for (int i = 0; i < rand.Next(30, 50); i++)
         {
             int value = rand.Next(-10, 20);
-            unconnectedVals.Add((value, rand.Next(2, 5)));
+            int count = rand.Next(2, 5);
+            if (unconnectedVals.Contains((value, count)))
+            {
+                value = rand.Next(-10, 20);
+                count = rand.Next(2, 5);
+            }
+            unconnectedVals.Add((value, count));
         }
         foreach (var value in unconnectedVals)
         {
@@ -60,14 +76,84 @@ public class FellmanBordTests
                 unconnectedVals.RemoveAt(2);
             }
         }
+        else
+        {
+            int weight1 = rand.Next(0, 10);
+            int weight2 = rand.Next(0, 10);
+            int weight3 = rand.Next(0, 10);
+            graph.AddEdge(unconnectedVals[0].value, unconnectedVals[1].value, weight1);
+            graph.AddEdge(unconnectedVals[0].value, unconnectedVals[2].value, weight2);
+            graph.AddEdge(unconnectedVals[2].value, unconnectedVals[1].value, weight3);
+            graph.AddEdge(unconnectedVals[1].value, unconnectedVals[0].value, weight1);
+            graph.AddEdge(unconnectedVals[2].value, unconnectedVals[0].value, weight2);
+            graph.AddEdge(unconnectedVals[1].value, unconnectedVals[2].value, weight3);
+            unconnectedVals[0] = (unconnectedVals[0].value, unconnectedVals[0].count);
+            unconnectedVals[1] = (unconnectedVals[1].value, unconnectedVals[1].count);
+            unconnectedVals[2] = (unconnectedVals[2].value, unconnectedVals[2].count);
+            connectedVals.Add(unconnectedVals[0]);
+            connectedVals.Add(unconnectedVals[1]);
+            connectedVals.Add(unconnectedVals[2]);
+            unconnectedVals.RemoveAt(0);
+            unconnectedVals.RemoveAt(1);
+            unconnectedVals.RemoveAt(2);
+        }
         while (unconnectedVals.Count > 0)
         {
             (int val, int count) thing = (-1000, -1000);
             while (thing == (-1000, -1000) || thing.count == 0)
             {
-                thing = SelectRandomly(connectedVals);
+                thing = SelectRandomly(connectedVals, rand.Next());
             }
-
+            graph.AddEdge(thing.val, unconnectedVals[0].value, rand.Next(0, 10));
+            graph.AddEdge(unconnectedVals[0].value, thing.val, rand.Next(0, 10));
+            connectedVals.Add((unconnectedVals[0].value, (unconnectedVals[0].count - 1)));
+            if (thing.count - 1 == 0)
+            {
+                connectedVals.Remove(thing);
+            }
+            else
+            {
+                connectedVals.Remove(thing);
+                connectedVals.Add((thing.val, thing.count - 1));
+            }
+            unconnectedVals.RemoveAt(0);
+        }
+        while (connectedVals.Count > 0)
+        {
+            (int val, int count) thing = (-1000, -1000);
+            (int val, int count) otherThing = (-1000, -1000);
+            while (thing == (-1000, -1000) || thing.count == 0)
+            {
+                thing = SelectRandomly(connectedVals, rand.Next());
+            }
+            while (otherThing == (-1000, -1000) || otherThing.count == 0)
+            {
+                otherThing = SelectRandomly(connectedVals, rand.Next());
+            }
+            while (!graph.AddEdge(thing.val, otherThing.val, rand.Next(0, 10))) 
+            {
+                thing = SelectRandomly(connectedVals, rand.Next());
+                otherThing = SelectRandomly(connectedVals, rand.Next());
+            }
+            graph.AddEdge(otherThing.val, thing.val, rand.Next(0, 10));
+            if (thing.count - 1 == 0)
+            {
+                connectedVals.Remove(thing);
+            }
+            else
+            {
+                connectedVals.Remove(thing);
+                connectedVals.Add((thing.val, thing.count - 1));
+            }
+            if (otherThing.count - 1 == 0)
+            {
+                connectedVals.Remove(otherThing);
+            }
+            else
+            {
+                connectedVals.Remove(otherThing);
+                connectedVals.Add((otherThing.val, otherThing.count - 1));
+            }
         }
         return (graph, isCycle);
     }
