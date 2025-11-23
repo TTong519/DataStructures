a83ecf917064dd77896a7260d3c034a717c429a8
tyler.tong <tyler.tong@GMR-118-1-1>
2025-11-22 16:02:18 -0800

    Implement HashMap functionality and rehashing support
    Fully implemented the `HashMap` class, including the indexer, `Keys`, `Add`, `Clear`, `Contains`, `CopyTo`, `GetEnumerator`, `Remove`, and `TryGetValue` methods. Added a `Rehash` method to dynamically resize the storage array when capacity is exceeded. Initialized `storage` in the constructor and replaced `NotImplementedException` placeholders with functional code. Added `System.Security.Cryptography` namespace for potential future use.

--- DataStructures/Lists/HashMap.cs
+++ DataStructures/Lists/HashMap.cs
@@ -12,9 +12,31 @@ namespace DataStructures.Lists
 {
     public class HashMap<Tkey, Tval> : IDictionary<Tkey, Tval>
     {
-        public Tval this[Tkey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Tval this[Tkey key] { 
+            get 
+            { 
+                foreach (var pair in storage[Math.Abs(key.GetHashCode() % storage.Length)]) 
+                { 
+                    if (pair.Key.Equals(key)) return pair.Value; 
+                } 
+                throw new Exception(); 
+            }  
+            set 
+            {
+                for(int i = 0; i < storage[Math.Abs(key.GetHashCode() % storage.Length)].Count; i++)
+                {
+                    var pair = storage[Math.Abs(key.GetHashCode() % storage.Length)].ElementAt(i);
+                    if (pair.Key.Equals(key))
+                    {
+                        storage[Math.Abs(key.GetHashCode() % storage.Length)].Remove(pair);
+                        storage[Math.Abs(key.GetHashCode() % storage.Length)].AddLast(new KeyValuePair<Tkey, Tval>(key, value));
+                        return;
+                    }
+                }
+                throw new Exception();
+            } }
 
-        public ICollection<Tkey> Keys => throw new NotImplementedException();
+        public ICollection<Tkey> Keys { get { List<Tkey> toReturn = new(); foreach (var bucket in storage) { } } };
         public ICollection<Tval> Values => throw new NotImplementedException();
         private LinkedList<KeyValuePair<Tkey, Tval>>[] storage = new LinkedList<KeyValuePair<Tkey, Tval>>[50];
 
@@ -28,65 +50,139 @@ namespace DataStructures.Lists
         {
             Comparer = comparer;
             Count = 0;
+            for(int i = 0; i < storage.Length; i++)
+            {
+                storage[i] = new LinkedList<KeyValuePair<Tkey, Tval>>();
+            }
         }
 
         public void Add(Tkey key, Tval value)
         {
-            //TODO: Rehash
             if (Count + 1 > storage.Length)
             {
-                throw new NotImplementedException();
+                Rehash();
             }
             int hash = Comparer.GetHashCode(key);
-            int index = Math.Abs(hash % storage.Length);
-
+            hash = Math.Abs(hash % storage.Length);
+            storage[hash].AddLast(new KeyValuePair<Tkey, Tval>(key, value));
+            Count++;
         }
 
         public void Add(KeyValuePair<Tkey, Tval> item)
         {
-            throw new NotImplementedException();
+            if (Count + 1 > storage.Length)
+            {
+                Rehash();
+            }
+            int hash = Comparer.GetHashCode(item.Key);
+            hash = Math.Abs(hash % storage.Length);
+            storage[hash].AddLast(item);
+            Count++;
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            Count = 0;
+            storage = new LinkedList<KeyValuePair<Tkey, Tval>>[50];
         }
 
         public bool Contains(KeyValuePair<Tkey, Tval> item)
         {
-            throw new NotImplementedException();
+            foreach(var pair in storage[Math.Abs(item.Key.GetHashCode() % storage.Length)])
+            {
+                return item.Value.Equals(pair.Value);
+            }
+            return false;
         }
 
         public bool ContainsKey(Tkey key)
         {
-            throw new NotImplementedException();
+            if (storage[Math.Abs(key.GetHashCode() % storage.Length)].Count == 0) return false;
+            return true;
         }
 
         public void CopyTo(KeyValuePair<Tkey, Tval>[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            foreach (var bucket in storage)
+            {
+                foreach (var pair in bucket)
+                {
+                    array[arrayIndex++] = pair;
+                }
+            }
         }
 
         public IEnumerator<KeyValuePair<Tkey, Tval>> GetEnumerator()
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < storage.Length; i++)
+            {
+                foreach(var pair in storage[i])
+                {
+                    yield return pair;
+                }
+            }
         }
 
         public bool Remove(Tkey key)
         {
-            throw new NotImplementedException();
+            foreach(var pair in storage[Math.Abs(key.GetHashCode() % storage.Length)])
+            {
+                if (pair.Key.Equals(key))
+                {
+                    storage[Math.Abs(key.GetHashCode() % storage.Length)].Remove(pair);
+                    Count--;
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool Remove(KeyValuePair<Tkey, Tval> item)
         {
-            throw new NotImplementedException();
+            foreach(var pair in storage[Math.Abs(item.Key.GetHashCode() % storage.Length)])
+            {
+                if (pair.Key.Equals(item.Key) && pair.Value.Equals(item.Value))
+                {
+                    storage[Math.Abs(item.Key.GetHashCode() % storage.Length)].Remove(pair);
+                    Count--;
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool TryGetValue(Tkey key, [MaybeNullWhen(false)] out Tval value)
         {
-            throw new NotImplementedException();
+            foreach (var pair in storage[Math.Abs(key.GetHashCode() % storage.Length)])
+            {
+                if (pair.Key.Equals(key))
+                {
+                    value = pair.Value;
+                    return true;
+                }
+            }
+            value = default;
+            return false;
         }
 
+        private void Rehash()
+        {
+            LinkedList<KeyValuePair<Tkey, Tval>>[] newStorage = new LinkedList<KeyValuePair<Tkey, Tval>>[storage.Length * 2];
+            for (int i = 0; i < newStorage.Length; i++)
+            {
+                newStorage[i] = new LinkedList<KeyValuePair<Tkey, Tval>>();
+            }
+            foreach (var bucket in storage)
+            {
+                foreach (var pair in bucket)
+                {
+                    int hash = Comparer.GetHashCode(pair.Key);
+                    hash = Math.Abs(hash % newStorage.Length);
+                    newStorage[hash].AddLast(pair);
+                }
+            }
+            storage = newStorage;
+        }
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
