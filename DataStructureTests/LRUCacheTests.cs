f9a2c1bf570d34ced17321e23d67885ca325f93c
Tyler.Tong <Tyler.Tong@GMR-118-2-3>
2025-12-26 19:06:30 -0800

    lru cache

---
+++ DataStructureTests/LRUCacheTests.cs
@@ -0,0 +1,56 @@
+namespace DataStructuresTests;
+using DataStructures.Lists;
+[TestClass]
+public class LRUCacheTests
+{
+    [TestMethod]
+    [DataRow(345638545)]
+    [DataRow(234657234)]
+    [DataRow(892355754)]
+    [DataRow(903498525)]
+    [DataRow(345257973)]
+    [DataRow(198598236)]
+    public void LRUCacheTest(int seed)
+    {
+        Random rand = new Random(seed);
+        int capacity = rand.Next(1, 1000);
+        LRUCache<int, int> lruCache = new(capacity);
+        Dictionary<int, int> reference = new();
+        List<int> keys = new();
+        for (int i = 0; i < capacity * 2; i++)
+        {
+            int key = rand.Next(2000);
+            int value = rand.Next();
+            lruCache.Put(key, value);
+            reference[key] = value;
+            if (!keys.Contains(key))
+            {
+                keys.Add(key);
+            }
+            if (keys.Count >= capacity)
+            {
+                keys.RemoveAt(keys.Count - 1);
+            }
+            Assert.AreEqual(keys.Count, lruCache.Count);
+            List<int> missVals = new List<int>();
+            for(int j = 0; j < capacity; j++)
+            {
+                int val = rand.Next();
+                while(missVals.Contains(val) || keys.Contains(val))
+                {
+                    val = rand.Next();
+                }
+                missVals.Add(val);
+            }
+            foreach(int val in missVals)
+            {
+                Assert.IsFalse(lruCache.TryGet(val, out int cachedValue));
+            }
+            foreach (var k in keys)
+            {
+                Assert.IsTrue(lruCache.TryGet(k, out int cachedValue));
+                Assert.AreEqual(reference[k], cachedValue);
+            }
+        }
+    }
+}
---
+++ DataStructures/Lists/LRUCache.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Lists
+{
+    public class LRUCache<Tkey, Tvalue>
+    {
+        LinkedList<KeyValuePair<Tkey, Tvalue>> values;
+        Dictionary<Tkey, LinkedListNode<KeyValuePair<Tkey, Tvalue>>> cache;
+        int capacity;
+        public int Count { get { return values.Count; } }
+        public LRUCache(int capacity)
+        {
+            this.capacity = capacity;
+            values = new LinkedList<KeyValuePair<Tkey, Tvalue>>();
+            cache = new Dictionary<Tkey, LinkedListNode<KeyValuePair<Tkey, Tvalue>>>();
+        }
+        public Tvalue Get(Tkey key)
+        {
+            if (!cache.ContainsKey(key))
+            {
+                throw new KeyNotFoundException();
+            }
+            var node = cache[key];
+            values.Remove(node);
+            values.AddFirst(node);
+            cache[key] = values.First;
+            return node.Value.Value;
+        }
+        public bool TryGet(Tkey key, out Tvalue value)
+        {
+            if (!cache.ContainsKey(key))
+            {
+                value = default(Tvalue);
+                return false;
+            }
+            value = Get(key);
+            return true;
+        }
+        public void Put(Tkey key, Tvalue value)
+        {
+            if (cache.ContainsKey(key))
+            {
+                var node = cache[key];
+                values.Remove(node);
+                values.AddFirst(node);
+                cache[key] = values.First;
+                node.Value = new KeyValuePair<Tkey, Tvalue>(key, value);
+                return;
+            }
+            if(values.Count == capacity)
+            {
+                var lruNode = values.Last;
+                values.RemoveLast();
+                cache.Remove(lruNode.Value.Key);
+            }
+            values.AddFirst(new KeyValuePair<Tkey, Tvalue>(key, value));
+            cache[key] = values.First;
+        }
+    }
+}
