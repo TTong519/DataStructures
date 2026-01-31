4598411ad5ae265f703948d1795147803b6188a4
Tyler Tong <tylertong519@gmail.com>
2026-01-30 19:03:08 -0800

    Initial Burst Trie structure and node classes
    Add abstract BurstNode base class and skeleton BurstTrie. Implement ContainerNode and InternalNode types with basic method stubs, establishing the polymorphic node hierarchy for the Burst Trie. Core logic for search and traversal is not yet implemented.

---
+++ DataStructures/Trees/ContainerNode.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Trees
+{
+    internal class ContainerNode : BurstNode
+    {
+        public BinarySearchTree<string> Data;
+        public override int Count => Data.Count;
+        public ContainerNode(BurstTrie parent) : base(parent)
+        {
+            Data = new BinarySearchTree<string>();
+        }
+
+        public override BurstNode Insert(string value, int index)
+        {
+            Data.Insert(value);
+            return this;
+        }
+
+        public override BurstNode Remove(string value, int index, out bool success)
+        {
+            success = Data.Remove(value);
+            return this;
+        }
+
+        public override BurstNode Search(string prefix, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void GetAll(List<string> output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataStructures/Trees/InternalNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Trees
+{
+    internal class InternalNode : BurstNode
+    {
+        public BurstNode[] children;
+        int count;
+        public override int Count => count;
+        public InternalNode(BurstTrie parent) : base(parent)
+        {
+            children = new BurstNode[27];
+            count = 0;
+        }
+
+        public override BurstNode Insert(string value, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BurstNode Remove(string value, int index, out bool success)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BurstNode Search(string prefix, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void GetAll(List<string> output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
