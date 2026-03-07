ee5c1b8442b471772d648895ebee8f226bf2ea53
tyler.tong <tyler.tong@GMR-118-1-1>
2026-03-06 19:02:03 -0800

    Initial B-Tree and BNode implementation (WIP)
    Add BNode<T> and BTree classes to start B-Tree data structure. Includes node expansion and insertion logic, with placeholders for splitting. Some methods are incomplete; this lays the foundation for further B-Tree development.

---
+++ DataStructures/Trees/BNode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Trees
+{
+    public class BNode<T> where T : IComparable<T>
+    {
+        public T[] Values;
+        public BNode<T>[] Children;
+        public int Degree { get; private set; }
+        public bool IsLeaf => Children.All(c => c == null);
+        public BNode(int degree)
+        {
+            Degree = degree;
+            Values = new T[degree - 1];
+            Children = new BNode<T>[degree];
+        }
+        public BNode<T> Split()
+        public bool Expand(T value)
+        {
+            if(Degree == 4)
+            {
+                return false;
+            }
+            T[] newValues = new T[Degree];
+            Values.CopyTo(newValues, 0);
+            newValues[Degree - 1] = value;
+            Children = new BNode<T>[Degree + 1];
+            Degree++;
+            return true;
+        }
+        public void Insert(T value)
+        {
+            if (IsLeaf)
+            {
+                Expand(value);
+                return;
+            }
+            for (int i = 0; i < Degree - 1; i++)
+            {
+                if (value.CompareTo(Values[i]) < 0)
+                {
+                    Children[i].Insert(value);
+                    return;
+                }
+            }
+            Children[Degree - 1].Insert(value);
+        }
+    }
+}
---
+++ DataStructures/Trees/BTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Trees
+{
+    public class BTree
+    {
+        public BNode<int> Root;
+        public BTree()
+        {
+            Root = null;
+        }
+        public void Insert(int value)
+        {
+            if (Root == null)
+            {
+                Root = new BNode<int>(value);
+                return;
+            }
+            Insert(Root, value);
+        }
+    }
+}
